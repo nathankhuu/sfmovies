86e21b096cdc43c07836bc65a30bcf054887ecd8
Nathan Khuu <nkhuu@berkeley.edu>
2018-05-24 11:26:20 -0700

    fix nit to collapse code to single line

--- db/migrations/20180524105631_add_name_to_movies.js
+++ db/migrations/20180524105631_add_name_to_movies.js
@@ -1,18 +1,14 @@
 'use strict';
 
 exports.up = (Knex) => {
-  return Knex.schema.table('movies', (table) => {
-    table.text('name');
-  })
+  return Knex.schema.table('movies', (table) => table.text('name'))
   .then(() => {
     return Knex.raw('ALTER TABLE movies ALTER COLUMN title DROP NOT NULL');
   });
 };
 
 exports.down = (Knex) => {
-  return Knex.schema.table('movies', (table) => {
-    table.dropColumn('name');
-  })
+  return Knex.schema.table('movies', (table) => table.dropColumn('name'))
   .then(() => {
     return Knex.raw('ALTER TABLE movies ALTER COLUMN title SET NOT NULL');
   });
