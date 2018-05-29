b86601f23f9e1bb7ec74570683893350986dd1a6
nathankhuu <nathankhuu@users.noreply.github.com>
2018-05-29 10:53:39 -0700

    refactor(movies): save name to database (#5)
    * refactor(movies): save name to database
    
    * add name key to movie model
    
    * fix to actually save name to database

--- test/plugins/features/movies/controller.test.js
+++ test/plugins/features/movies/controller.test.js
@@ -12,6 +12,7 @@ describe('movie controller', () => {
       return Controller.create(payload)
       .then((movie) => {
         expect(movie.get('title')).to.eql(payload.title);
+        expect(movie.get('name')).to.eql(payload.title);
       });
     });
 
